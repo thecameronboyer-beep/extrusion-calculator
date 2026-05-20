9c2ed62055ddcd1eca1e9729307fb566fddfb651
Cameron <thecameronboyer@gmail.com>
2026-05-20 16:28:30 -0700

    Clean up long press timer

--- src/components/ResultRow.jsx
+++ src/components/ResultRow.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function ResultRow({
   label,
@@ -37,6 +37,8 @@ export default function ResultRow({
       event.preventDefault();
     }
   }
+
+  useEffect(() => clearPressTimer, []);
 
   return (
     <div
